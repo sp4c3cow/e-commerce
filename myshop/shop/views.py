2acab5b616f798a275c36dfd7470231f3673a86e
4mn3s1a <cow@co.ws>
2020-07-29 12:40:24 +0200

    some html templates was added

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -4,7 +4,7 @@ from .models import Category, Product
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    products = Product.objects.filter(availability=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
@@ -12,7 +12,7 @@ def product_list(request, category_slug=None):
     return render(request, 'shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    product = get_object_or_404(Product, id=id, slug=slug, availability=True)
     
     return render(request, 'shop/product/detail.html', {'product': product})
 
