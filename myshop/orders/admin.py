4aceeaf6238afcff8502b2a56aa79a0d7381bb60
4mn3s1a <cow@co.ws>
2020-08-07 19:19:39 +0200

    Custom admin order view page

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,12 +1,18 @@
-from django.contrib import admin
-from .models import OrderItem, Order
 import csv
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from django.contrib import admin
+from .models import OrderItem, Order
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
 
 def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
@@ -36,7 +42,7 @@ class OrderAdmin(admin.ModelAdmin):
     actions = [export_to_csv]
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     
